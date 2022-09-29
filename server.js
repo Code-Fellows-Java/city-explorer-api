1f362f7fc6f5d3e0929735213d53176b8c695a95
Ian Forrester <ian.forrester46@gmail.com>
2022-09-29 02:41:43 +0000

    added movie function

--- server.js
+++ server.js
@@ -46,6 +46,33 @@ async function getWeather(request, response) {
   }
 }
 
+app.get('/movies', getMovies);
+
+class Movie {
+  constructor(movie) {
+    this.title = movie.original_title;
+    this.overview = movie.overview;
+    this.average_votes = movie.vote_average;
+    this.total_votes = movie.vote_count;
+    this.image_url = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+    this.popularity = movie.popularity;
+    this.released_on = movie.release_date;
+  }
+}
+
+async function getMovies(request, response) {
+  const searchQuery = request.query.searchQuery;
+  const url1 = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`;
+
+  try {
+    const movies = await axios.get(url1);
+    const movieArray = movies.data.results.map(movie => new Movie(movie));
+    response.status(200).send(movieArray);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 
 
 
